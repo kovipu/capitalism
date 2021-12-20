95d7edc7930f08b1e1c92b546fa64968014575d6
Konsta Purtsi <konsta@identio.fi>
2021-12-20 21:44:36 +0200

    Add graphQL base.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::sync::Arc;
+
+use actix_web::{web, App, Error, HttpResponse, HttpServer};
+use juniper::http::graphiql::graphiql_source;
+use juniper::http::GraphQLRequest;
+
+mod nordnet;
+use nordnet::schema::{create_schema, Schema};
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    // Create Juniper schema
+    let schema = Arc::new(create_schema());
+
+    // Start http server
+    HttpServer::new(move || {
+        App::new()
+            .data(schema.clone())
+            .route("/graphiql", actix_web::web::get().to(graphiql))
+            .route("/graphql", actix_web::web::post().to(graphql))
+    })
+    .bind("127.0.0.1:8081")?
+    .run()
+    .await
+}
+
+async fn graphql(
+    st: web::Data<Arc<Schema>>,
+    data: web::Json<GraphQLRequest>,
+) -> Result<HttpResponse, Error> {
+    let body = web::block(move || {
+        let res = data.execute_sync(&st, &());
+        serde_json::to_string(&res)
+    })
+    .await?;
+
+    Ok(HttpResponse::Ok()
+        .content_type("application/json")
+        .body(body))
+}
+
+async fn graphiql() -> HttpResponse {
+    let html = graphiql_source("http://localhost:8081/graphql", None);
+
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(html)
 }
---
+++ backend/src/nordnet/schema.rs
@@ -0,0 +1,46 @@
+use juniper::GraphQLObject;
+use juniper::{EmptyMutation, EmptySubscription, FieldResult, RootNode};
+
+#[derive(GraphQLObject)]
+struct NordnetAccount {
+    /// The account identifier (id) of the account. The id is unique within the session. Not applicable for partners
+    accid: Option<i32>, // TODO: use i64
+    /// The Nordnet account number
+    accno: i32, // TODO: use i64
+    /// Account alias can be set on Nordnet by the end user
+    alias: String,
+    /// Account type identifier
+    atyid: Option<i32>,
+    /// Description to why the account is blocked. The language specified in the request is used in this reply so it can be displayed to the end user
+    blocked_reason: Option<String>,
+    /// True if this is the default account
+    default: bool,
+    /// True if the account is blocked. No queries can be made to this account
+    is_blocked: Option<bool>,
+    /// Translated account type.
+    account_type: String,
+}
+
+pub struct QueryRoot;
+
+#[juniper::graphql_object]
+impl QueryRoot {
+    fn nordnetAccounts() -> FieldResult<Vec<NordnetAccount>> {
+        Ok(vec![NordnetAccount {
+            accid: Some(1),
+            accno: 1,
+            alias: "".to_string(),
+            atyid: Some(1),
+            blocked_reason: Some("".to_string()),
+            default: true,
+            is_blocked: Some(false),
+            account_type: "".to_string(),
+        }])
+    }
+}
+
+pub type Schema = RootNode<'static, QueryRoot, EmptyMutation, EmptySubscription>;
+
+pub fn create_schema() -> Schema {
+    Schema::new(QueryRoot {}, EmptyMutation::new(), EmptySubscription::new())
+}
