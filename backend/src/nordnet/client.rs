9c1ef693d28c9fe0c8db71402f731065075ad6f1
Konsta Purtsi <konsta@identio.fi>
2021-12-27 14:19:21 +0200

    WIP Nordnet client.

--- backend/src/nordnet/client.rs
+++ backend/src/nordnet/client.rs
@@ -1,12 +1,13 @@
 use awc;
-use awc::error::{SendRequestError};
+use awc::error::SendRequestError;
 
 const API_URL: &str = "https://www.nordnet.fi/";
 
 pub async fn get_accounts() -> Result<i32, SendRequestError> {
     dbg!("requesting");
     let client = awc::Client::new();
-    let accounts = client.get(format!("{}/api/accounts", API_URL))
+    let accounts = client
+        .get(format!("{}/api/accounts", API_URL))
         .send()
         .await?;
 
--- backend/src/nordnet/mod.rs
+++ backend/src/nordnet/mod.rs
@@ -1,2 +1,2 @@
-pub mod schema;
 pub mod client;
+pub mod schema;
